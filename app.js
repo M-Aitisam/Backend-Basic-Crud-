f833564aa9430de41d83ec625164052e27f2256a
M-Aitisam <aitisamahmed24@gmail.com>
2025-02-07 21:28:37 +0500

    Update the code working on  creting crud with pages

--- app.js
+++ app.js
@@ -81,9 +81,17 @@
 
 const express =  require('express');
 const app = express();
+const path =  require("path");
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname , 'public')));
+app.set("view engine" , "ejs");
 
 app.get("/" , function(req , res){
-    res.send("Welcome")
+    res.render("view")
 })
-
+app.get("/read" , function(req , res){
+    res.render("read")
+})
 app.listen(3000);
